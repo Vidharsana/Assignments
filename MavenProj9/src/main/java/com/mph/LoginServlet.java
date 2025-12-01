379a747a93ffecf1ae76dc3e4b298f5c33dc377d
Vidharsana <vidharsanasundharraj@gmail.com>
2025-12-01 17:26:04 +0530

    Dec 1-Day 9

---
+++ MavenProj9/src/main/java/com/mph/LoginServlet.java
@@ -0,0 +1,57 @@
+package com.mph;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		
+		
+		String username=request.getParameter("uname");
+		String pwd=request.getParameter("pwd");
+		
+		
+		if(username.equals("vidhu")&&pwd.equals("vidhu"))
+			out.println(username+" authenticated successfully");
+		else
+			out.println("Login failed");
+	}
+	
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ MavenProj9/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h2><%= "Login Form" %></h2>
+<form action="LoginServlet" method="get">
+<label>Username : </label>
+<input type ="text" value="uname">
+<label>Password : </label>
+<input type ="password" value="pwd" required><br><br>
+<input type ="submit" value="Login">
+</form>
+</body>
+</html>
